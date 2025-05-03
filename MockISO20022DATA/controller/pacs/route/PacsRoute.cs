3fc8d096c0bfa0c39b64b45be635cf030f7753e6
Isaac Agyei <agyeis@hotmail.com>
2025-05-03 11:20:44 -0600

    add new routes for pacs.002.001.15

--- MockISO20022DATA/controller/pacs/route/PacsRoute.cs
+++ MockISO20022DATA/controller/pacs/route/PacsRoute.cs
@@ -70,4 +70,61 @@ public class PacsController() : Controller
       return StatusCode(500, $"An error occurred while generating the XML documents: {ex.Message}");
     }
   }
+
+  [HttpPost]
+  [Route("00200115/json/{amountToGenerate}")]
+  public IActionResult FIToFIPaymentStatusReportV15MessageJson([FromBody] JsonObject requestBody, Int16 amountToGenerate)
+  {
+    FIToFIPaymentStatusReportV15_JSON classInstance = new();
+    List<JsonObject> responseList = [];
+
+    if (amountToGenerate < 0)
+    {
+      return BadRequest("Please indicate the amount of mock data objects you want generated. Must be greater than 0.");
+    }
+
+    try
+    {
+      for (Int16 i = 0; i < amountToGenerate; i++)
+      {
+        JsonObject updatedObject = GenerateJson.UpdateJsonValues(requestBody, classInstance._methodNameList, classInstance);
+        responseList.Add(updatedObject);
+      }
+
+      string jsonResponse = JsonSerializer.Serialize(responseList, new JsonSerializerOptions { WriteIndented = true });
+      return Ok(jsonResponse);
+    }
+    catch (Exception ex)
+    {
+      return StatusCode(500, $"An error occurred while generating the JSON objects: {ex.Message} ");
+    }
+  }
+
+  [HttpPost]
+  [Route("00200115/xml/{amountToGenerate}")]
+  public IActionResult FIToFIPaymentStatusReportV15MessageXml([FromBody] XmlDocument requestBody, Int16 amountToGenerate)
+  {
+    FIToFIPaymentStatusReportV15_XML classInstance = new();
+    List<string> responseList = [];
+
+    if (amountToGenerate < 0)
+    {
+      return BadRequest("Please indicate the amount of mock XML documents you want generated. Must be greater than 0.");
+    }
+
+    try
+    {
+      for (Int16 i = 0; i < amountToGenerate; i++)
+      {
+        string updatedXml = GenerateXml.UpdateXmlInnerText(requestBody, classInstance._methodNameList, classInstance);
+        responseList.Add(updatedXml);
+      }
+
+      return Ok(responseList);
+    }
+    catch (Exception ex)
+    {
+      return StatusCode(500, $"An error occurred while generating the XML documents: {ex.Message}");
+    }
+  }
 }
